2e48b2bf24f3f05a37f485867393962806fb72e6
Leonardo Vissossi <leonardovissossi98@gmail.com>
2024-08-05 11:45:39 -0300

    aula 03 pratica 08

---
+++ aula-03/pratica-08/script.js
@@ -0,0 +1,33 @@
+const numeros = [
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+    Math.round(Math.random() * 100),
+];
+
+const resultado = document.getElementById("resultado");
+
+let total = 0;
+
+for( const numero of numeros){
+    total += numero;
+
+}
+
+    resultado.innerHTML = `Temos um total de ${numeros.length} numeros e o resultado da soma é ${total}`;
